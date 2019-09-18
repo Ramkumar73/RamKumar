83d57ce206f25d6fca80c444e2bca54072b77249
Ramkumar73 <ramgym73@gmail.com>
2019-09-18 16:32:01 +0530

    SecondProgram

--- src/Maths/Calculation_2.java
+++ src/Maths/Calculation_2.java
@@ -1,5 +1,11 @@
 package Maths;
 
-public class Calculation_2 {
-
+public class Calculation_2
+{
+	public static void main(String[] args) 
+	{
+		int a = 8;
+		int b = 4;
+		System.out.println(a-b);
+	}
 }
